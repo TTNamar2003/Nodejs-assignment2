34e9a354eaa54203de29293eb7547b6a0400e161
TTNamar2003 <amar.singh@tothenew.com>
2025-03-19 09:57:55 +0530

    indentation formating of code base

--- frontend-assignment2/app/page.tsx
+++ frontend-assignment2/app/page.tsx
@@ -2,7 +2,11 @@
 export default function Home() {
   return (
     <div >
-       hello world
+       <p>for question 1, hit routes : users/search</p>
+       <p>for question 2, hit routes : /students</p>
+       <p>for question 3, hit routes : /users for home page</p>
+       <p>for question 3, hit routes : /users/about for about page </p>
+       <p>for question 3, hit routes : /users/add-user to add user page </p>
     </div>
   );
 }
